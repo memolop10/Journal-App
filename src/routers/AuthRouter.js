45b03f601acfccd8a2c65e754439e83d4bec3ff7
memolop10 <inglopezvargas@gmail.com>
2021-07-04 22:13:51 -0500

    styles in LoginScreen

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -6,22 +6,24 @@ import RegisterScreen from '../components/auth/RegisterScreen'
 
 const AuthRouter = () => {
     return (
-        <div>
-           <Switch>
-                <Route 
-                    exact
-                    path="/auth/login"
-                    component={ LoginScreen }
-                />   
+        <div className="auth__main">
+            <div className="auth__box-container">
+                <Switch>
+                    <Route 
+                        exact
+                        path="/auth/login"
+                        component={ LoginScreen }
+                    />   
 
-                <Route
-                    exact
-                    path="/auth/register"
-                    component={ RegisterScreen }
-                />
+                    <Route
+                        exact
+                        path="/auth/register"
+                        component={ RegisterScreen }
+                    />
 
-                <Redirect to="/auth/login"/>
-            </Switch> 
+                    <Redirect to="/auth/login"/>
+                </Switch> 
+            </div>
         </div>
     )
 }
