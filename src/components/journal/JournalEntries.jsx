17a3da9c0f30b3675b42d58d39c5bab747196d02
memolop10 <inglopezvargas@gmail.com>
2021-07-09 14:06:33 -0500

    changes exists

--- src/components/journal/JournalEntries.jsx
+++ src/components/journal/JournalEntries.jsx
@@ -5,7 +5,7 @@ import JournalEntry from './JournalEntry';
 
 const JournalEntries = () => {
 
-    const entries = [1,2,3,4,6,7];
+    const entries = [1,2,3,4,5];
     
     return (
         <div className="journal__entries">
