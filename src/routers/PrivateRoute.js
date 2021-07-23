6e8295c628cfdce068ca7610b64500b32a1fa232
memolop10 <inglopezvargas@gmail.com>
2021-07-22 23:52:52 -0500

    implementacion private routes

---
+++ src/routers/PrivateRoute.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router'
+import PropTypes from 'prop-types'
+
+const PrivateRoute = ({
+    isAuthenticated,
+    component:Component,
+    ...rest
+}) => {
+
+    console.log(rest.location.pathname) 
+    localStorage.setItem('lastPath',rest.location.pathname)
+
+    return (
+        <Route { ...rest }
+            component={ (props) => (
+                (isAuthenticated) ?
+                    ( <Component { ...props } /> ) :
+                    ( <Redirect to="/auth/login" /> )
+            )}    
+        
+        
+        />
+            
+    )
+}
+
+PrivateRoute.propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired,
+    component: PropTypes.func.isRequired
+}
+
+export default PrivateRoute
